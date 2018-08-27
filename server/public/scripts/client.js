af6a5a9e0724b8d631d63114f77ace82545162c7
Unknown <Isaac.overlander@gmail.com>
2018-08-27 14:39:35 -0500

    Form sends data to the client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,12 @@ const myApp = angular.module('myApp', [] );
 
 myApp.controller('AppController', function($http) {
     const vm = this;
+    
+    vm.sendMessage= function(){
+        vm.messageToSend = {
+            name: vm.name,
+            message: vm.message
+        };
+        
+    }
 });
