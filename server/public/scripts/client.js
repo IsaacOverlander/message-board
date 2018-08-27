91cef8c10c70d5f5b62d378a518e3323cf531fd3
Unknown <Isaac.overlander@gmail.com>
2018-08-27 14:52:01 -0500

    Created client side GET route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,7 +3,7 @@ const myApp = angular.module('myApp', [] );
 myApp.controller('AppController', function($http) {
     const vm = this;
     
-    vm.sendMessage= function(){
+    vm.sendMessage = function(){
         vm.messageToSend = {
             name: vm.name,
             message: vm.message
@@ -18,5 +18,16 @@ myApp.controller('AppController', function($http) {
             console.log('Error posting message:', error);
             alert('Error posting message');
         });//End POST
+    }
+
+    function getMessages() {
+        $http({
+            method: 'GET',
+            url: '/'
+        }).then(function (response) {
+            console.log(response);
+        }).catch(function (error) {
+            console.log('Error getting messages:', error);
+        });// End GET
     }
 });
